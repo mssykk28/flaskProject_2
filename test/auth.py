b0de07f1d47d76c9717f05fc141f16317d78009e
masashi <mss.nt.2828@gmail.com>
2022-03-23 09:26:34 +0900

    keyの削除

--- test/auth.py
+++ test/auth.py
@@ -2,8 +2,8 @@ import http.client
 
 conn = http.client.HTTPSConnection("dev-2jun8kx9.us.auth0.com")
 
-payload = "{\"client_id\":\"uvH95PfQpY3wzcufeQ8pkUssiJ7C09oZ\"," \
-          "\"client_secret\":\"tTaFIjDxhZCR1KdYisLBDJz2KDzPlnp-Slnv4JyIcMLEXxR6gTPhbjC__YVM2o-p\"," \
+payload = "{\"client_id\":\"hoge\"," \
+          "\"client_secret\":\"hoge\"," \
           "\"audience\":\"http://127.0.0.1:5000/auth\",\"grant_type\":\"client_credentials\"}"
 
 headers = {"content-type": "application/json"}
